6a1c705d59053a4008d607cfa0c6eaed3f926823
LuchKan-Git-Hub <kanluch172@gmail.com>
2025-02-02 17:07:05 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 2040
+API_HASH = 'b18441a1ff607e10a989891a5462e627'
+API_TOKEN = 'your_token'
+
+# FusionBrainAPI
+API_KEY = 'api key fushion brain'
+SECRET_KEY = 'scrret key fushion brain too'
